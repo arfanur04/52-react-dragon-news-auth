c957c64c37a4b547e34fbb2071373c5306e910f9
arfanur04 <arfanur04@gmail.com>
2024-04-11 18:07:59 +0600

    52-7

--- src/layouts/Roots.jsx
+++ src/layouts/Roots.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 const Roots = () => {
 	return (
-		<div className="max-w-6xl mx-auto font-poppins">
+		<div className="max-w-6xl mx-auto font-poppins px-2">
 			<Outlet></Outlet>
 		</div>
 	);
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -3,13 +3,14 @@ import moment from "moment";
 
 const Header = () => {
 	return (
-		<div>
+		<div className="text-center">
 			<img
+				className="mx-auto"
 				src={logo}
 				alt=""
 			/>
 			<p>Journalism Without Fear or Favour</p>
-			<p className="text-xl">{moment().format("dddd, MMMM D, YYYY1")}</p>
+			<p className="text-xl">{moment().format("dddd, MMMM D, YYYY")}</p>
 		</div>
 	);
 };
