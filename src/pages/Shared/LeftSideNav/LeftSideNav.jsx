f36ba42ba1019b7e218233899c2a7d5b94c9ecfc
arfanur04 <arfanur04@gmail.com>
2024-04-12 18:33:39 +0600

    52-9 Left Side Navigation

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+
 const LeftSideNav = () => {
+	const [categories, setCategories] = useState([]);
+
+	useEffect(() => {
+		fetch(`categories.json`)
+			.then((res) => res.json())
+			.then((data) => setCategories(data))
+			.catch((err) => console.error("err", err));
+	}, []);
+
 	return (
-		<div>
-			<h2>This is LeftSideNav</h2>
+		<div className="space-y-6">
+			<h2 className="text-2xl">All Categories</h2>
+			{categories?.map((category) => (
+				<NavLink
+					to={`/category/${category.id}`}
+					className="block ml-4 text-xl font-semibold"
+					key={category.id}
+				>
+					{category.name}
+				</NavLink>
+			))}
 		</div>
 	);
 };
